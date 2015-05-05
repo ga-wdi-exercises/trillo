aea3786fa3cda1527a51551a3669ff907f7d9727
Jesse Shawl <jesse@jshawl.com>
2015-05-05 15:54:17 -0400

    add pseudocode

---
+++ static/application.js
@@ -0,0 +1,17 @@
+// TrilloView
+  // create new cards 
+  // handle form submission
+  // put existing cards into
+  // either done or todo lists.
+
+// Trillo Object
+  // stores a reference to
+  // all cards
+
+// CardView
+  // displaying the card
+  // allow completion check
+
+// Card Object
+  // description: string 
+  // completed: boolean
