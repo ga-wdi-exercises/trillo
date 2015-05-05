2f400e335dc5300dbe97546ae395b7a9f0bf59ce
Jesse Shawl <jesse@jshawl.com>
2015-05-05 18:12:49 -0400

    add completion toggle

--- static/js/models/card.js
+++ static/js/models/card.js
@@ -3,5 +3,13 @@ var Card = function( description ){
   // description: string 
   this.description = description
   // completed: boolean
+  this.completed = false
+  this.id = trillo.cards.length
   trillo.cards.push( this )
+}
+
+Card.prototype = {
+  save: function(){
+    trillo.cards[this.id] = this	
+  }
 }
--- static/js/views/card.js
+++ static/js/views/card.js
@@ -1,9 +1,25 @@
 // CardView
 var CardView = function( card ){
-  console.log( card )
   // displaying the card
   var li = document.createElement("li")
+  li.className = "card"
   li.innerHTML = card.description
+  var input = document.createElement("input")
+  input.type = "checkbox"
+  input.checked = card.completed
+  input.className = "finish"
+  input.addEventListener("click", function(){
+    card.completed = input.checked ? true : false
+    card.save()
+    trilloView.render()
+  })
+  li.appendChild( input )
   return li
   // allow completion check
+}
+
+CardView.prototype = {
+  toggleCompleted: function( event ){
+    alert("yeah")
+  }
 }
