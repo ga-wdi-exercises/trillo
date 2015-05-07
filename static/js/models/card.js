16c7055955e4a3658dbf10f14165ad86ba02ce12
tsetoung <jtmoua@yahoo.com>
2015-05-07 07:29:13 -0400

    forgot to save models/cards

--- static/js/models/card.js
+++ static/js/models/card.js
@@ -1,7 +1,12 @@
 // Card Object
 var Card = function( description ){
-  // description: string 
+  // description: string
   this.description = description
   // completed: boolean
   trillo.cards.push( this )
 }
+
+//the Card allows us to create the new card description and pushes it into the
+//MODELS/trillo.js empty array.
+
+//.push(this) is refering to /models/trillo.js "this.cards = []". 
