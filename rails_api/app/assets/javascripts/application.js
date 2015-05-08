692a3089611831361968332a1c5c1a9b59fdd5ab
RobertAKARobin <robertgfthomas@gmail.com>
2015-05-08 13:13:21 -0400

    Added starter js

--- rails_api/app/assets/javascripts/application.js
+++ rails_api/app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  trilloModel = new Trillo();
+  trilloView = new TrilloView(trilloModel);
+});
